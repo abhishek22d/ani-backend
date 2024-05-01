17241aecf414b666323ca9a1082de271e7445df3
vikas <vikaslr1718@gmail.com>
2024-05-01 19:16:20 +0530

    initial commit

--- src/main/java/com/ani/backend/Configuration/MailOtpConfiguration.java
+++ src/main/java/com/ani/backend/Configuration/MailOtpConfiguration.java
@@ -1,5 +1,4 @@
 package com.ani.backend.Configuration;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
